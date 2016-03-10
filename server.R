8c6c63255ed404c5c6bb1f0718d18550d0961765
Ji Yoon <jung.wldbs@gmail.com>
2016-03-10 10:08:19 -0800

    server ui comment added
    q

--- server.R
+++ server.R
@@ -7,16 +7,24 @@ source("scripts/male_vs_female.R")
 
 divorce_data <- read.csv("data/divorce.csv")
 shinyServer(function(input, output) {
+  
+  # Allows user to find divorve rate based on age group
   output$age_vs_divorce <- renderPlotly({
     divorce_vs_age_graph(input$age)
   })
+  
+  # Allows user to find divorce rate based on year, age group
   output$divorce <- renderPlotly({
     cat <- get(input$cat)
     pie_chart(divorce_data, input$year, cat, input$age_group)
   })
+  
+  # Allows user to see marriage rate per region based on year
   output$married <- renderPlotly({
     married_state_map(input$interest_year)
   })
+  
+  # Allows user to find male and female marriage rate based on age group
   output$male_vs_female <- renderPlotly({
     male_female_graph(input$age_range)
   })
--- ui.R
+++ ui.R
@@ -3,10 +3,18 @@ library(plotly)
 # install.packages("shinythemes")
 library(shinythemes)
 
+# Shiny UI
 shinyUI(navbarPage("Menu",
                    theme = shinytheme("Flatly"),
+                   
+                   # Tabs for seperate graphs
                    tabPanel("Not Married by Gender",
+                            
+                            # Page title
                             titlePanel("Not Married by Gender"),
+                            
+                            # Shiny widget, controls age group of interest
+                            # Adds explanation
                             sidebarPanel(
                               radioButtons("age_range", label = h4("Choose an Age Group"),
                                            choices = list("25-34" = "all_2534",
@@ -16,13 +24,19 @@ shinyUI(navbarPage("Menu",
                               includeMarkdown("detail/explain_gender.md")
                             ),
                             
+                            # Render male and female marriage rate line graph
                             mainPanel(
                               plotlyOutput("male_vs_female")
                             )
                    ),
                    
                    tabPanel("Marriage Rate by Region",
-                            titlePanel("Percentage of people married regional data"),
+                            
+                            # Page title
+                            titlePanel("Percentage of people married: Regional data"),
+                            
+                            # Shiny widget, controls year of interest
+                            # Includes map explanation
                             sidebarPanel(
                               selectInput("interest_year", label = h3("Select year"),
                                           choices = list("1960", "1970", "1980", "1990", 
@@ -30,6 +44,8 @@ shinyUI(navbarPage("Menu",
                                           selected = "1960"),
                               includeMarkdown("detail/explain_map.md")
                             ),
+                            
+                            # Render marriage rate regional map
                             mainPanel(
                               plotlyOutput("married")
                             )
@@ -37,7 +53,13 @@ shinyUI(navbarPage("Menu",
 
                    
                    tabPanel("Divorce by Factors", 
+                            
+                            # Page title
                             titlePanel("Divorce by Factors"),
+                            
+                            # Shiny widget, controls year, category(education, wealth),
+                            # and age of interest
+                            # Includes map explanation
                             sidebarPanel(
                               sliderInput("year", "Year", min = 2000, max = 2012, 
                                           value = 1, sep = ""),
@@ -49,19 +71,28 @@ shinyUI(navbarPage("Menu",
                                            selected = '3544')
                               
                             ),
+                            
+                            # Render divorce rate pie chart by factors
                             mainPanel(
                               plotlyOutput("divorce")
                             )
                    ),
                    
                    tabPanel("Age vs. Divorce",
+                            
+                            # Page title 
                             titlePanel("Age vs Divorce"),
+                            
+                            # Shiny widget, controls age group of interest
+                            # Includes graph explanation
                             sidebarPanel(
                               radioButtons("age", label = h4("Choose an Age Group"),
                                            choices = list("35-44" = "all_3544", "45-54" = "all_4554"),
                                            selected = "all_3544"),
                               includeMarkdown("detail/explain_age_divorce.md")
                             ),
+                            
+                            # Render divorce rate by age group
                             mainPanel(
                               plotlyOutput("age_vs_divorce")
                             )
